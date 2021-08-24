60a2672c7f64f22b2587795f8a2028c3ec2d686a
casdea <casdea@gmail.com>
2021-08-23 23:43:48 -0300

    Autenticacao, Autorizacao e padrão MVC

--- ServLets/cliente-webservice/src/br/com/alura/cliente/ClienteWebService.java
+++ ServLets/cliente-webservice/src/br/com/alura/cliente/ClienteWebService.java
@@ -9,7 +9,7 @@ public class ClienteWebService {
 
 	public static void main(String[] args) throws ClientProtocolException, IOException {
 		String conteudo = Request.Post("http://localhost:8080/gerenciador/empresas")
-		.addHeader("accept","application/xxml")		
+		.addHeader("Accept","application/xxml")		
 		.execute()
 		.returnContent()
 		.asString();		
