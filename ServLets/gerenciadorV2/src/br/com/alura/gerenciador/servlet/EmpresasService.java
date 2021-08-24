0ecead1f20c80708343a51830db9686256e31c9b
casdea <casdea@gmail.com>
2021-08-23 23:01:45 -0300

    Autenticacao, Autorizacao e padrão MVC

--- ServLets/gerenciadorV2/src/br/com/alura/gerenciador/servlet/EmpresasService.java
+++ ServLets/gerenciadorV2/src/br/com/alura/gerenciador/servlet/EmpresasService.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
 
 import br.com.alura.gerenciador.model.Banco;
 import br.com.alura.gerenciador.model.Empresa;
@@ -21,16 +23,29 @@ import br.com.alura.gerenciador.model.Empresa;
 @WebServlet("/empresas")
 public class EmpresasService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		List<Empresa> empresas = new Banco().getEmpresas();
-		
-		Gson gson = new Gson();
-		String json = gson.toJson(empresas);
-		
-		response.setContentType("application/json");
-		response.getWriter().print(json);
+
+		int tipo = 1;
+
+		if (tipo == 1) {
+			XStream xStream = new XStream();
+			xStream.alias("empresas", Empresa.class);
+			String xml = xStream.toXML(empresas);
+			
+			response.setContentType("application/xml");
+			response.getWriter().print(xml);
+
+		} else {
+			Gson gson = new Gson();
+			String json = gson.toJson(empresas);
+
+			response.setContentType("application/json");
+			response.getWriter().print(json);
+		}
 	}
 
 }
