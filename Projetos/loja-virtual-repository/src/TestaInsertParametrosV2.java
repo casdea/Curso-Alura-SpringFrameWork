abcec06091cf6622b59134ee1583c3ae48a36932
casdea <casdea@gmail.com>
2021-08-25 13:35:48 -0300

    jdbc dao persistencia

--- Projetos/loja-virtual-repository/src/TestaInsertParametrosV2.java
+++ Projetos/loja-virtual-repository/src/TestaInsertParametrosV2.java
@@ -1,9 +1,10 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class TestaInsertParametros {
+public class TestaInsertParametrosV2 {
 
 	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
@@ -16,10 +17,15 @@ public class TestaInsertParametros {
 		String nome = "JACA";
 		String descricao = "SUCO DE JACA";
 		
-		Statement stm = connection.createStatement();
-		stm.execute("INSERT INTO PRODUTO (NOME, DESCRICAO) Values ('"+nome+"','"+descricao+"')",
+		PreparedStatement stm = connection.prepareStatement(
+				"INSERT INTO PRODUTO (NOME, DESCRICAO) Values (?, ?)",
 				Statement.RETURN_GENERATED_KEYS);
 
+		stm.setString(1, nome);
+		stm.setString(2, descricao);
+		
+		stm.execute();
+		
 		ResultSet resultado = stm.getGeneratedKeys();
 
 		while (resultado.next()) {
