2311088d2798ce3afc789e2497ebefd5e4a13f48
casdea <casdea@gmail.com>
2021-08-28 22:47:11 -0300

    Spring data jpa: Repositorios, Consultas, Projeções e Specificactions

--- Projetos/Spring-data/src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ Projetos/Spring-data/src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import br.com.alura.spring.data.orm.Cargo;
 import br.com.alura.spring.data.repository.CargoRepository;
 import br.com.alura.spring.data.service.CrudCargoService;
+import br.com.alura.spring.data.service.CrudFuncionarioService;
+import br.com.alura.spring.data.service.CrudUnidadeService;
 
 @SpringBootApplication
 public class SpringDataApplication implements CommandLineRunner {
@@ -16,9 +18,14 @@ public class SpringDataApplication implements CommandLineRunner {
 	private boolean system = true;
 
 	private final CrudCargoService crudCargoService;
+	private final CrudUnidadeService crudUnidadeService;
+	private final CrudFuncionarioService crudFuncionarioService;
 
-	public SpringDataApplication(CrudCargoService crudCargoService) {
+	public SpringDataApplication(CrudCargoService crudCargoService, CrudUnidadeService crudUnidadeService,
+			CrudFuncionarioService crudFuncionarioService) {
 		this.crudCargoService = crudCargoService;
+		this.crudUnidadeService = crudUnidadeService;
+		this.crudFuncionarioService = crudFuncionarioService;
 	}
 
 	public static void main(String[] args) {
@@ -36,7 +43,19 @@ public class SpringDataApplication implements CommandLineRunner {
 			System.out.println("2 - Atualizar Cargo");
 			System.out.println("3 - Visualizar Cargo");
 			System.out.println("4 - Excluir Cargo");
-			System.out.println("5 - Exibir Todos");
+			System.out.println("5 - Exibir Todos Cargos");
+
+			System.out.println("6 - Inserir Unidade");
+			System.out.println("7 - Atualizar Unidade");
+			System.out.println("8 - Visualizar Unidade");
+			System.out.println("9 - Excluir Unidade");
+			System.out.println("10 - Exibir Todas Unidades");
+
+			System.out.println("11 - Inserir Funcioanrio");
+			System.out.println("12 - Atualizar Funcionario");
+			System.out.println("13 - Visualizar Funcionario");
+			System.out.println("14 - Excluir Funcionario");
+			System.out.println("15 - Exibir Todos Funcionaro");
 
 			int action = scanner.nextInt();
 			if (action == 1) {
@@ -49,6 +68,27 @@ public class SpringDataApplication implements CommandLineRunner {
 				crudCargoService.deletar(scanner);
 			} else if (action == 5) {
 				crudCargoService.visualizarTodos(scanner);
+			} else if (action == 6) {
+				crudUnidadeService.salvar(scanner);
+			} else if (action == 7) {
+				crudUnidadeService.atualizar(scanner);
+			} else if (action == 8) {
+				crudUnidadeService.visualizar(scanner);
+			} else if (action == 9) {
+				crudUnidadeService.deletar(scanner);
+			} else if (action == 10) {
+				crudUnidadeService.visualizarTodos(scanner);
+			} else if (action == 11) {
+				crudFuncionarioService.salvar(scanner);
+			} else if (action == 12) {
+				crudFuncionarioService.atualizar(scanner);
+			} else if (action == 13) {
+				crudFuncionarioService.visualizar(scanner);
+			} else if (action == 14) {
+				crudFuncionarioService.deletar(scanner);
+			} else if (action == 15) {
+				crudFuncionarioService.visualizarTodos(scanner);
+
 			} else {
 				system = false;
 			}
