a9bba717dac1be9db0546a566441f734e3eccda4
casdea <casdea@gmail.com>
2021-09-02 00:25:34 -0300

    Spring MVC - Crie um Web app com Thymeleaf e BooStrap

--- Projetos/spring-mvc/src/main/java/br/com/alura/springmvc/controller/HomeController.java
+++ Projetos/spring-mvc/src/main/java/br/com/alura/springmvc/controller/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import br.com.alura.springmvc.model.Pedido;
 import br.com.alura.springmvc.repository.PedidoRepository;
 import br.com.alura.springmvc.repository.PedidoRepositoryJpa;
 
@@ -20,6 +21,13 @@ public class HomeController {
 	@GetMapping("/home")
 	public String home(Model model) 
 	{
+		Pedido pedido = new Pedido();
+		pedido.setDescricaoProduto("Descricao Produto 1");
+		pedido.setNomeProduto("Nome Produto 1");
+		pedido.setUrlImagem("Url Imagem 1");
+		pedido.setUrlProduto("Url Produto 1");
+		pedidoRepository.save(pedido);
+		
 		//model.addAttribute("pedidos",PedidoRepositoryJpa.findAll());
 		model.addAttribute("pedidos",pedidoRepository.findAll());
 		
