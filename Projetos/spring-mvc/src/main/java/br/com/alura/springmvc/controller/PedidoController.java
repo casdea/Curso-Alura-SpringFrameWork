1b1e9dd6bb2ec9f1a54c2843981c2190d27097cd
casdea <casdea@gmail.com>
2021-09-02 08:07:30 -0300

    Spring MVC - Crie um Web app com Thymeleaf e BooStrap

--- Projetos/spring-mvc/src/main/java/br/com/alura/springmvc/controller/PedidoController.java
+++ Projetos/spring-mvc/src/main/java/br/com/alura/springmvc/controller/PedidoController.java
@@ -1,13 +1,20 @@
 package br.com.alura.springmvc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.alura.springmvc.dto.RequisicaoNovoPedido;
+import br.com.alura.springmvc.repository.PedidoRepository;
+
 @Controller
 @RequestMapping("pedido")
 public class PedidoController {
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;   
 	
 	@GetMapping("formulario")
 	public String formulario()
@@ -16,8 +23,10 @@ public class PedidoController {
 	}
 
 	@PostMapping("novo")
-	public String novo()
+	public String novo(RequisicaoNovoPedido requisicaoNovoPedido)
 	{
+		pedidoRepository.save(requisicaoNovoPedido.toPedido());
+		
 		return "pedido/formulario";
 	}
 
