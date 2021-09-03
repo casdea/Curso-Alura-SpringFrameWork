fb83b4e8e2069777446ea8888e9e4df350b25599
casdea <casdea@gmail.com>
2021-09-03 07:58:45 -0300

    Spring MVC - Autentica cm Spring security, API Rest e AJAX

--- Projetos/spring-mvc-aut-rest-ajax/src/main/java/br/com/alura/springmvc/security/WebSecurityConfig.java
+++ Projetos/spring-mvc-aut-rest-ajax/src/main/java/br/com/alura/springmvc/security/WebSecurityConfig.java
@@ -1,18 +1,28 @@
 package br.com.alura.springmvc.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private DataSource dataSource;
+	
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
@@ -20,6 +30,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest().authenticated()
 			.and()
 			.formLogin(form -> form.loginPage("/login")
+			.defaultSuccessUrl("/home",true) 
 			.permitAll())
 			.logout(logout -> logout.logoutUrl("/logout"));
 
@@ -36,17 +47,35 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.permitAll();
 				*/
 	}
-
-	@Bean
-	@Override
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("adm")
-				.password("adm")
-				.roles("ADM")
-				.build();
-
-		return new InMemoryUserDetailsManager(user);
+	
+	@Override 
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		/*UserDetails user =
+				 User.builder()
+				 	.username("ope")
+				 	.password(encoder.encode("ope"))
+				 	.roles("USER")
+				 	.build();
+		
+		auth.jdbcAuthentication()
+			.dataSource(dataSource)
+			.passwordEncoder(encoder)
+			.withUser(user);
+		*/	
+		auth.jdbcAuthentication()
+		.dataSource(dataSource)
+		.passwordEncoder(encoder);
 	}
+
+	/*
+	 * @Bean
+	 * 
+	 * @Override public UserDetailsService userDetailsService() { UserDetails user =
+	 * User.withDefaultPasswordEncoder() .username("adm") .password("adm")
+	 * .roles("ADM") .build();
+	 * 
+	 * return new InMemoryUserDetailsManager(user); }
+	 */
 }
