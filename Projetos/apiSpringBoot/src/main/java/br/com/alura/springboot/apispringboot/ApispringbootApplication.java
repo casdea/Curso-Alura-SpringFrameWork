b58d119449c36a2acf0efc951dbb376f72feb99c
casdea <casdea@gmail.com>
2021-09-06 19:42:55 -0300

    Spring MVC - Autentica cm Spring security, API Rest e AJAX

---
+++ Projetos/apiSpringBoot/src/main/java/br/com/alura/springboot/apispringboot/ApispringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.springboot.apispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApispringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApispringbootApplication.class, args);
+	}
+
+}
