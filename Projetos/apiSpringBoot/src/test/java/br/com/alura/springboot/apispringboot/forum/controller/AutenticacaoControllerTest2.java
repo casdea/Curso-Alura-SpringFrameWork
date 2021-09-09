318f670538dbcb01dbd3fad752d8cb47e051d829
casdea <casdea@gmail.com>
2021-09-08 22:21:07 -0300

    Spring Boot e teste - Profiles Testes e Deploy

---
+++ Projetos/apiSpringBoot/src/test/java/br/com/alura/springboot/apispringboot/forum/controller/AutenticacaoControllerTest2.java
@@ -0,0 +1,62 @@
+package br.com.alura.springboot.apispringboot.forum.controller;
+
+import java.net.URI;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class AutenticacaoControllerTest2 {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+
+	@Test
+	public void deveriaDevolver400CasoDadosDeAutenticacaoEstejamIncorretos() throws Exception {
+		URI uri = new URI("/auth");
+		String json = "{\"email\":\"invalido@email.com\",\"senha\":\"123456\"}";
+		
+		mockMvc
+		.perform(MockMvcRequestBuilders
+				.post(uri)
+				.content(json)
+				.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(MockMvcResultMatchers
+				.status()
+				.is(403));
+	}
+
+}
